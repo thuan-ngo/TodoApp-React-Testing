391b4fd05740b7be2dd35f8a731a7beb42a4597f
Ngo Nhu Thuan <thuan-ngo@cybozu.vn>
2023-08-27 16:44:29 +0700

    Setup todo app & unit test

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header({ title }) {
+  return (
+    <>
+      <h1 title="Header" className="header">
+        {title}
+      </h1>
+    </>
+  );
+}
