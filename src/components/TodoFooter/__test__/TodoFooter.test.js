3e0186e38cec9b2b45a6fd7a4a526f3998760fdc
Ngo Nhu Thuan <thuan-ngo@cybozu.vn>
2023-10-12 21:51:02 +0700

    Add unittest for Todo & TodoFooter folder

---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import TodoFooter from "../TodoFooter";
+import { BrowserRouter } from "react-router-dom";
+
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+};
+
+describe("TodoFooter", () => {
+  it("should render the correct amount of incomplete tasks", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const pElement = screen.getByText(/5 tasks left/i);
+    expect(pElement).toBeInTheDocument();
+  });
+
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const pElement = screen.getByText(/1 task left/i);
+    expect(pElement).toBeInTheDocument();
+  });
+});
